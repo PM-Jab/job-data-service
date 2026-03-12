4aecc7f7710541395bf19637b3e8a8c23331e16b
Possawee Jab <possawee.m@arise.tech>
2026-03-13 02:10:48 +0700

    spring initializr

---
+++ src/main/java/com/assignment/job_data_service/JobDataServiceApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.job_data_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobDataServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobDataServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/assignment/job_data_service/JobDataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.job_data_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobDataServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
